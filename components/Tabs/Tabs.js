82767cd6e83c5fe97a62f7021b1aca0ca2c74f71
unknown <38929577+meginks@users.noreply.github.com>
2019-02-01 11:28:49 -0600

    got content to appear with appropriate tabs - mvp complete :)

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,16 +12,16 @@ class TabLink {
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelectorAll('.card');
+      this.cards = document.querySelectorAll(`.card`);
+      //console.log(this.cards);
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
       this.cards = document.querySelectorAll(`.card[data-tab='${this.tabData}']`);
+    //  console.log(this.cards);
     }
-  
-
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     this.cards = Array.from(this.cards).map(card => new TabCard(card));
-
+    console.log(this.cards);
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', () => this.selectTab());
   };
@@ -51,7 +51,8 @@ class TabLink {
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    this.cardElement = document.querySelector(".card"); 
+    this.cardElement = cardElement;
+   // console.log(this.cardElement);
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
